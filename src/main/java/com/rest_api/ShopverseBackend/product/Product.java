48c762585081822ad508263331718f6b15988300
abraham <abraham@pinedadev.com>
2023-05-15 03:36:18 +0300

    added custom entry points to product entity

--- src/main/java/com/rest_api/ShopverseBackend/product/Product.java
+++ src/main/java/com/rest_api/ShopverseBackend/product/Product.java
@@ -2,6 +2,7 @@ package com.rest_api.ShopverseBackend.product;
 
 import jakarta.persistence.*;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -29,12 +30,29 @@ public class Product
   @NotBlank
   private String name;
 
+  @Column(nullable = true)
+  private String img;
+
   @Column(nullable = false)
   private String description;
 
-  @Column (nullable = false)
+  @Column(nullable = false)
   private String[] categories;
 
+  @Column(nullable = false)
+  private String[] colors;
+
+  @Column(nullable = false)
+  private int[] sizes;
+
+  @Column(nullable = false)
+  private int price;
+
+  @Column(nullable = true)
+  private int reviews;
+
+  @Column(nullable = true)
+  private int stars;
 
   private LocalDateTime createdAt = LocalDateTime.now();
 }
