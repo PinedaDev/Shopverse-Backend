75d26b68df8f881dfb60b9157746ed6635a40465
abraham <abraham@pinedadev.com>
2023-05-26 09:05:18 +0300

    typo fix

--- src/main/java/com/rest_api/ShopverseBackend/SecurityConfig/CORSConfig.java
+++ src/main/java/com/rest_api/ShopverseBackend/SecurityConfig/CORSConfig.java
@@ -11,7 +11,7 @@ public class CORSConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-            .allowedOrigins("https://shopverse-store.netlify.app")
+            .allowedOrigins("https://shopverse-store.netlify.app", "http://localhost:2345", "http://localhost:5173")
             .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .exposedHeaders("Access-Control-Allow-Origin");
--- src/main/java/com/rest_api/ShopverseBackend/product/ProductController.java
+++ src/main/java/com/rest_api/ShopverseBackend/product/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
+@CrossOrigin(origins = {"http://localhost:5137"})
 @RestController
 @RequestMapping("/api/v1/products")
 @AllArgsConstructor
