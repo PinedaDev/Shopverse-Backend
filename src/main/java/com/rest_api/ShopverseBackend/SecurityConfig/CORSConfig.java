d8f1af58f6e35b6c2e07da6bb6f440fe5f60030e
abraham <abraham@pinedadev.com>
2023-06-02 10:49:12 +0300

    cors config updated

--- src/main/java/com/rest_api/ShopverseBackend/SecurityConfig/CORSConfig.java
+++ src/main/java/com/rest_api/ShopverseBackend/SecurityConfig/CORSConfig.java
@@ -1,19 +1,28 @@
 package com.rest_api.ShopverseBackend.SecurityConfig;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
-public class CORSConfig implements WebMvcConfigurer {
-  @Override
-  public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**")
-            .allowedOrigins("https://shopverse-store.netlify.app", "http://localhost:2345", "http://localhost:5173")
-            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-            .allowedHeaders("*")
-            .exposedHeaders("Access-Control-Allow-Origin");
+public class CORSConfig {
+
+  private static final String GET = "GET";
+  private static final String POST = "POST";
+  private static final String PUT = "PUT";
+  private static final String DELETE = "DELETE";
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173", "https://shopverse-backend-zsuc.onrender.com",
+                        "https://shopverse-store.netlify.app/").allowedMethods(GET, POST, PUT, DELETE)
+                .allowedHeaders("*").allowedOriginPatterns("*").allowCredentials(true);
+      }
+    };
   }
 }
--- src/main/java/com/rest_api/ShopverseBackend/product/ProductController.java
+++ src/main/java/com/rest_api/ShopverseBackend/product/ProductController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
-@CrossOrigin(origins = {"http://localhost:5137"})
 @RestController
 @RequestMapping("/api/v1/products")
 @AllArgsConstructor
